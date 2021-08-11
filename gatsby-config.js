f8203afaeced94601dc4b8bb5a09f36d52650071
rkr-dev <object.prototype.js@gmail.com>
2021-08-11 03:45:37 -0400

    Configured gatsby
     - plugins
     - env variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,23 +1,55 @@
+require("dotenv").config({
+  path: `.env`,
+})
+
+
 module.exports = {
+  flags:{DEV_SSR:true},
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "portfolio-rinilkunhiraman",
+    title: `Rinil Kunhiraman | Web Developer`,
+    description: `Website portfolio of Rinil Kunhiraman`,
+    author: `Rinil Kunhiraman`,
+    image: ``,
+    keywords:['portfolio','javascript','gatsby'],
+    siteUrl: "https://www.rinilkunhiraman.dev",
+    social:{
+      twitter:`rinilkunhiraman`,
+      facebook:`rinilkunhiraman`,
+      instagram:`rinilkunhiraman`,
+      linkedin:`rinilkunhiraman`,
+    }
   },
   plugins: [
-    "gatsby-plugin-styled-components",
-    "gatsby-plugin-image",
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-offline`,
     {
       resolve: "gatsby-plugin-google-analytics",
       options: {
-        trackingId: "",
+        trackingId: "UA-172851032-1",
+        head: false,
+        anonymize: true,
+        respectDNT: true,
+        exclude: [],
+        pageTransitionDelay: 0,
+        defer: false,
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
       },
     },
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
     {
       resolve: "gatsby-plugin-manifest",
-      options: {
-        icon: "src/images/icon.png",
+      options:{
+        name: `Rinil Kunhiraman`,
+        short_name: `RK`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `${__dirname}/src/assets/icon.png` ,
       },
     },
     "gatsby-plugin-sharp",
@@ -25,10 +57,29 @@ module.exports = {
     {
       resolve: "gatsby-source-filesystem",
       options: {
-        name: "images",
-        path: "./src/images/",
+        name: `assets`,
+        path: `${__dirname}/src/assets/`,
       },
-      __key: "images",
+      __key: "assets",
+    },
+    {
+      resolve: `gatsby-source-strapi`,
+      options: {
+        apiURL: `https://evening-cove-34160.herokuapp.com`,
+        queryLimit: 1000, // Default to 100
+        contentTypes: ["jobs", "projects", "blogs", "services","products"],
+        singleTypes: [`about`],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+            `Roboto\:400,700`,
+            `Open Sans`,
+        ],
+        display:'swap'
+      },
     },
   ],
 };
