79af4f7c1a6a52046334940c0e366e874d58affb
rkr-dev <object.prototype.js@gmail.com>
2021-08-13 22:26:09 -0400

    Blog Page
     - Blog page
     - Blog Components

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,8 +1,38 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'gatsby';
+import { GatsbyImage } from 'gatsby-plugin-image';
 import * as styles from './Blog.module.css';
 
-export const Blog = () => {
-    return (
-        <h1>Blog</h1>
-    )
-}
+export const Blog = ({ id, title, image, date, category, slug, desc }) => {
+  return (
+    <Link to={`/blogs/${slug}`} key={id}>
+      <article className={styles.blog}>
+        {image && (
+          <GatsbyImage
+            image={image.localFile.childImageSharp.gatsbyImageData}
+            className={styles.blogImg}
+          />
+        )}
+        <div className={styles.blogCard}>
+          <h4>{title}</h4>
+          <p>{desc}</p>
+          <div className={styles.blogFooter}>
+            <p>{category}</p>
+            <p>{date}</p>
+          </div>
+        </div>
+      </article>
+    </Link>
+  );
+};
+
+Blog.propTypes = {
+  id: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  image: PropTypes.object.isRequired,
+  date: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
+  slug: PropTypes.string.isRequired,
+  desc: PropTypes.string.isRequired,
+};
--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import cx from 'classnames';
+import { Link } from 'gatsby';
+import { Blog,Title } from '../../components';
 import * as styles from './Blogs.module.css';
 
-export const Blogs = () => {
-  return <h1>Blogs</h1>;
+export const Blogs = ({blogs,title,showLink}) => {
+  return (
+    <section className="section">
+      {console.log('Blogs:',blogs)}
+        <Title title={title}/>
+        <div className={cx(styles.sectionCenter, styles.blogsCenter)}>
+            {blogs.map(({node}) => (
+                <Blog key={node.id} {...node}/>
+            ))}
+        </div>
+        {showLink && <Link to="/blog" className={cx(styles.btn, styles.centerBtn)}>blog</Link>}
+    </section>
+  );
 };
-
-
